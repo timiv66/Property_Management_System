2979acc9127562f9569e0e557fa4961d45d9b678
Timi Verissimo <timiv9@gmail.com>
2024-10-13 17:05:37 -0400

    Login pane visual setup

--- src/Property/Property_FX.java
+++ src/Property/Property_FX.java
@@ -1,18 +1,124 @@
 package Property;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Property_FX extends Application{
+	Font titleFont = new Font("Stencil",25);
+	Font btnFont = new Font("Elephant",18);
+	Font lblFont = new Font("Impact",25);
 	
 	public static void main(String[] args) {
-		
+		launch();
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage mainStage) throws Exception {
 		// TODO Auto-generated method stub
+		Pane p1 = new Pane();
+		Scene t = new Scene(p1,400,350);
+		t.setRoot(login(t));
+		mainStage.setScene(t);
+		mainStage.show();
+		mainStage.setTitle("ATM");
 		
+		
+		
+	}
+	
+	public Pane login(Scene t) {
+		Label title = new Label("Property Management System");
+		title.setAlignment(Pos.CENTER);
+		title.setFont(titleFont);
+		title.setTranslateX(3);
+		
+		Line line = new Line();
+		line.setStartX(0); 
+		line.setEndX(400); 
+		line.setStartY(30);
+		line.setEndY(30);
+		line.setSmooth(true);
+		line.setStroke(Color.RED);
+		line.setStrokeWidth(5);
+		
+		Label loginLbl = new Label("Login");
+		loginLbl.setTranslateY(40);
+		loginLbl.setTranslateX(170);
+		loginLbl.setFont(btnFont);
+		
+		//Email field
+		Label emailLbl = new Label("Email:");
+		emailLbl.setTranslateX(85);
+		emailLbl.setTranslateY(70);
+		emailLbl.setFont(lblFont);
+		
+		TextField emailTxtF = new TextField();
+		emailTxtF.setTranslateX(150);
+		emailTxtF.setTranslateY(75);
+		
+		//Password field
+		Label passLbl = new Label("Password:");
+		passLbl.setTranslateX(85);
+		passLbl.setTranslateY(110);
+		passLbl.setFont(lblFont);
+		
+		TextField passTxtF = new TextField();
+		passTxtF.setTranslateX(195);
+		passTxtF.setTranslateY(115);
+		
+		
+		//User choosing if they are a tenant or a landlord
+		ToggleGroup group = new ToggleGroup();
+
+		RadioButton tenantBtn = new RadioButton("Tenant");
+		tenantBtn.setToggleGroup(group);
+		tenantBtn.setTranslateX(120);
+		tenantBtn.setTranslateY(150);
+				
+		RadioButton landlordBtn = new RadioButton("Landlord");
+		landlordBtn.setToggleGroup(group);
+		landlordBtn.setTranslateX(210);
+		landlordBtn.setTranslateY(150);
+		
+		//Login button
+		Button loginBtn = new Button("Login");
+		loginBtn.setTranslateX(170);
+		loginBtn.setTranslateY(180);
+		
+		//New Tenant button
+		Button newTenantBtn = new Button("New Tenant");
+		newTenantBtn.setTranslateX(100);
+		newTenantBtn.setTranslateY(235);
+		
+		
+		//New Landlord button
+		Button newLandlordBtn = new Button("New Landlord");
+		newLandlordBtn.setTranslateX(210);
+		newLandlordBtn.setTranslateY(235);
+		
+		Pane loginPane = new Pane();
+		
+		BackgroundFill background_fill = new BackgroundFill(Color.DARKCYAN,CornerRadii.EMPTY, Insets.EMPTY); 
+		Background background = new Background(background_fill);
+		loginPane.setBackground(background);
+		
+		loginPane.getChildren().addAll(title,line,loginLbl,emailLbl,emailTxtF,passLbl,passTxtF,tenantBtn,landlordBtn,loginBtn,newTenantBtn,newLandlordBtn);
+		return loginPane;
 	}
 
 }
